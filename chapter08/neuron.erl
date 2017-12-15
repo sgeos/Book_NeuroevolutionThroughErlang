4ea0b5918ae1872e067166247b6001de51ea99ca
Brendan A R Sechter <b-sechter@unext.jp>
2017-12-15 20:34:47 +0900

    Chapter 8 source code complete.  Compiles without warnings or errors, but will be tested in the next chapter.

---
+++ chapter08/neuron.erl
@@ -0,0 +1,236 @@
+%% This source code and work is provided and developed by DXNN Research Group WWW.DXNNResearch.COM
+%%
+% Copyright (C) 2012 by Gene Sher, DXNN Research Group, CorticalComputer@gmail.com
+% All rights reserved.
+%
+% This code is licensed under the version 3 of the GNU General Public License. Please see the LICENSE
+% file that accompanies this project for the terms of use.
+
+-module( neuron ).
+%-compile( export_all ).
+-export(
+  [
+    gen/2,
+    prep/1,
+    loop/8,
+    dot/3,
+    fanout/2,
+    flush_buffer/0,
+    perturb_IPIdPs/1,
+    perturb_IPIdPs/3,
+    perturb_weights/3,
+    sat/3
+  ]
+).
+-include( "records.hrl" ).
+-define( DELTA_MULTIPLIER, math:pi() * 2 ).
+-define( SAT_LIMIT, math:pi() * 2 ).
+-define( RO_SIGNAL, math:pi() * 2 ).
+
+% When gen/2 is executed, it spawns the neuron element and immediately begins to wait for its initial
+% state message from the exoself. Once the state message arrives, the neuron sends out the default
+% forward signals to any elements in its ro_ids list, if any. Afterwards, prep drops into the
+% neuron's main loop.
+gen( ExoSelf_PId, Node ) ->
+  spawn( Node, ?MODULE, prep, [ ExoSelf_PId ] ).
+
+% !!! Using cryptographically strong seed instead of erlang:now() and random:seed().
+% !!! reference: (see How to generate cryptographically strong seeds with the new rand library)
+% https://hashrocket.com/blog/posts/the-adventures-of-generating-random-numbers-in-erlang-and-elixir
+%
+prep( ExoSelf_PId ) ->
+  << I1:32/unsigned-integer, I2:32/unsigned-integer, I3:32/unsigned-integer >> =
+    crypto:strong_rand_bytes( 12 ),
+  rand:seed( exsplus, { I1, I2, I3 } ),
+  receive
+    { ExoSelf_PId, { Id, Cx_PId, AF, Input_PIdPs, Output_PIds, RO_PIds } } ->
+      fanout( RO_PIds, { self(), forward, [ ?RO_SIGNAL ] } ),
+      loop( Id, ExoSelf_PId, Cx_PId, AF, { Input_PIdPs, Input_PIdPs }, Output_PIds, RO_PIds, 0 )
+  end.
+
+% The neuron process waits for vector signals from all the processes that it’s connected from, taking
+% the dot product of the input and weight vectors, and then adding it to the accumulator. Once all
+% the signals from Input_PIds are received, the accumulator contains the dot product to which the
+% neuron then adds the bias and executes the activation function. After fanning out the output
+% signal, the neuron again returns to waiting for incoming signals. When the neuron receives the
+% { ExoSelf_PId, get_backup } message, it forwards to the exoself its full MInput_PIdPs list, and its
+% Id. The MInput_PIdPs contains the modified, tuned and most effective version of the input_idps. The
+% neuron process also accepts the weight_backup signal, when receiving it, the neuron saves to
+% process dictionary the current MInput_PIdPs. When the neuron receives the weight_restore signal, it
+% reads back from the process dictionary the stored Input_PIdPs, and switches over to using it as its
+% active Input_PIdPs list. When the neuron receives the weight_perturb signal from the exoself, it
+% perturbs the weights by executing the perturb_IPIdPs/1 function, which returns the
+% updated/perturbed weight list. Finally, the neuron can also accept a reset_prep signal, which makes
+% the neuron flush its buffer in the off chance that it has a recursively sent to it signal in its
+% inbox. After flushing its buffer, the neuron waits for the exoself to send it the reset signal, at
+% which point the neuron, now fully refreshed after the flush_buffer/0, outputs a default forward
+% signal to its recursively connected elements (ro_ids), if any, and then drops back into its main
+% receive loop.
+loop(
+  Id,
+  ExoSelf_PId,
+  Cx_PId,
+  AF,
+  { [ { Input_PId, Weights } | Input_PIdPs ], MInput_PIdPs },
+  Output_PIds,
+  RO_PIds,
+  Acc
+) ->
+  receive
+    { Input_PId, forward, Input } ->
+      Result = dot( Input, Weights, 0 ),
+      loop(
+        Id,
+        ExoSelf_PId,
+        Cx_PId,
+        AF,
+        { Input_PIdPs, MInput_PIdPs },
+        Output_PIds,
+        RO_PIds,
+        Result + Acc
+      );
+    { ExoSelf_PId, weight_backup } ->
+      put( weights, MInput_PIdPs ),
+      loop(
+        Id,
+        ExoSelf_PId,
+        Cx_PId,
+        AF,
+        { [ { Input_PId, Weights } | Input_PIdPs ], MInput_PIdPs },
+        Output_PIds,
+        RO_PIds,
+        Acc
+      );
+    { ExoSelf_PId, weight_restore } ->
+      RInput_PIdPs = get( weights ),
+      loop( Id, ExoSelf_PId, Cx_PId, AF, { RInput_PIdPs, RInput_PIdPs }, Output_PIds, RO_PIds, Acc );
+    { ExoSelf_PId, weight_perturb } ->
+      PInput_PIdPs = perturb_IPIdPs( MInput_PIdPs ),
+      loop(
+        Id,
+        ExoSelf_PId,
+        Cx_PId,
+        AF,
+        { [ { Input_PId, Weights } | Input_PIdPs ], PInput_PIdPs },
+        Output_PIds,
+        RO_PIds,
+        Acc
+      );
+    { ExoSelf, reset_prep } ->
+      neuron:flush_buffer(),
+      ExoSelf ! { self(), ready },
+      receive
+        { ExoSelf, reset } ->
+          fanout( RO_PIds, { self(), forward, [ ?RO_SIGNAL ] } )
+      end,
+      loop(
+        Id,
+        ExoSelf_PId,
+        Cx_PId,
+        AF,
+        { MInput_PIdPs, MInput_PIdPs },
+        Output_PIds,
+        RO_PIds,
+        0
+      );
+    { ExoSelf_PId, get_backup} ->
+      ExoSelf_PId ! { self(), Id, MInput_PIdPs },
+      loop(
+        Id,
+        ExoSelf_PId,
+        Cx_PId,
+        AF,
+        { [ { Input_PId, Weights } | Input_PIdPs ], MInput_PIdPs },
+        Output_PIds,
+        RO_PIds,
+        Acc
+      );
+    { ExoSelf_PId, terminate} ->
+      io:format( "Neuron: ~p has terminated.~n", [ self() ] ),
+      ok
+  end;
+loop( Id, ExoSelf_PId, Cx_PId, AF, { [ Bias ], MInput_PIdPs }, Output_PIds, RO_PIds, Acc ) ->
+  Output = functions:AF( Acc + Bias ),
+  [ Output_PId ! { self(), forward, [ Output ] } || Output_PId <- Output_PIds ],
+  loop( Id, ExoSelf_PId, Cx_PId, AF, { MInput_PIdPs, MInput_PIdPs }, Output_PIds, RO_PIds, 0 );
+loop( Id, ExoSelf_PId, Cx_PId, AF, { [], MInput_PIdPs }, Output_PIds, RO_PIds, Acc ) ->
+  Output = functions:AF(Acc),
+  [ Output_PId ! { self(), forward, [ Output ] } || Output_PId <- Output_PIds ],
+  loop( Id, ExoSelf_PId, Cx_PId, AF, { MInput_PIdPs, MInput_PIdPs }, Output_PIds, RO_PIds, 0).
+
+% The dot/3 function accepts an input vector and a weight list, and computes the dot product of the
+% two vectors.
+dot( [ I | Input ], [ W | Weights ], Acc ) ->
+  dot( Input, Weights, I * W + Acc );
+dot( [], [], Acc ) ->
+  Acc;
+dot( [], [ Bias ], Acc ) ->
+  Acc + Bias.
+
+% The fanout/2 function fans out th Msg to all the PIds in its list.
+fanout( [ Pid | Pids ], Msg ) ->
+  Pid ! Msg,
+  fanout( Pids, Msg );
+fanout( [], _Msg ) ->
+  true.
+
+% The flush_buffer/0 cleans out the element's inbox.
+flush_buffer() ->
+  receive
+    _ ->
+      flush_buffer()
+    after 0 ->
+      done
+  end.
+
+% The perturb_IPIdPs/1 function calculates the probablity with which each neuron in the Input_PIdPs
+% is chosen to be perturbed. The probablity is based on the total number of weights in the
+% Input_PIdPs list, with the actual mutation probablity equatling to the inverse of square root of
+% total number of weights. The perturb_IPIdPs/3 function goes through each weights block and calls
+% the perturb_weights/3 to perturb the weights.
+perturb_IPIdPs( Input_PIdPs ) ->
+  Tot_Weights = lists:sum( [ length( Weights ) || { _Input_PId, Weights } <- Input_PIdPs ] ),
+  MP = 1 / math:sqrt( Tot_Weights ),
+  perturb_IPIdPs( MP, Input_PIdPs, [] ).
+
+perturb_IPIdPs( MP, [ { Input_PId, Weights } | Input_PIdPs ], Acc ) ->
+  U_Weights = perturb_weights( MP, Weights, [] ),
+  perturb_IPIdPs( MP, Input_PIdPs, [ { Input_PId, U_Weights } | Acc ] );
+perturb_IPIdPs( MP, [ Bias ], Acc ) ->
+  U_Bias = case rand:uniform() < MP of
+    true ->
+      sat( ( rand:uniform() - 0.5 ) * ?DELTA_MULTIPLIER + Bias, -?SAT_LIMIT, ?SAT_LIMIT );
+    false ->
+      Bias
+  end,
+  lists:reverse( [ U_Bias | Acc ] );
+perturb_IPIdPs( _MP, [], Acc ) ->
+  lists:reverse( Acc ).
+
+% The perturb_weights/3 function is the function that actually goes through each weight block, and
+% perturbs each weight with a probablity of MP. If the weight is chosen to be perturbed, the
+% perturbation intensity is chosen uniformly between -Pi and Pi.
+perturb_weights( MP, [ W | Weights ], Acc ) ->
+  U_W = case rand:uniform() < MP of
+    true ->
+      sat( ( rand:uniform() - 0.5 ) * ?DELTA_MULTIPLIER + W, -?SAT_LIMIT, ?SAT_LIMIT );
+    false ->
+      W
+  end,
+  perturb_weights( MP, Weights, [ U_W | Acc ] );
+perturb_weights( _MP, [], Acc ) ->
+  lists:reverse( Acc ).
+
+% The sat/3 function simply ensures that the Val is neither less than min or greater than max. When
+% used with synaptic weights (or other parameters), this function makes sure that the synaptic
+% weights get saturated at the Min and Max values, rather than growing in magnitude without bound.
+sat( Val, Min, Max ) ->
+  if
+    Val < Min ->
+      Min;
+    Max < Val ->
+      Max;
+    true ->
+      Val
+  end.
+
---
+++ chapter08/polis.erl
@@ -0,0 +1,264 @@
+%% This source code and work is provided and developed by DXNN Research Group WWW.DXNNResearch.COM
+%%
+% Copyright (C) 2012 by Gene Sher, DXNN Research Group, CorticalComputer@gmail.com
+% All rights reserved.
+%
+% This code is licensed under the version 3 of the GNU General Public License. Please see the LICENSE
+% file that accompanies this project for the terms of use.
+
+-module(polis).
+-export(
+  [
+    % API
+      start/0,
+      start/1,
+      stop/0,
+      init/2,
+      create/0,
+      reset/0,
+      sync/0,
+    % gen_server callbacks
+      init/1,
+      handle_call/3,
+      handle_cast/2,
+      handle_info/2,
+      terminate/2,
+      code_change/3
+  ]
+).
+-behavior(gen_server).
+-include("records.hrl").
+
+%  --- --- --- Configuration Options --- --- ---
+
+% The MODS list contains the names of the processes, functions, or other databases that also need to
+% be executed and started when we start our neuroevolutionary platform. In the same manner, when we
+% have created a new public scape, we can add a scape_summary tuple with this scape's information to
+% the PUBLIC_SCAPES list, so that it is initialized and started with the system. The state record for
+% the polis has all the elements needed to track the currently active mods and public scapes, which
+% were either present during the startup of the neuroevolutionary platform, or latter added while the
+% polis was already online.
+-record(state, {active_mods=[], active_scapes=[]}).
+-record(scape_summary, {address, type, parameters=[]}).
+-define(MODS, []).
+-define(PUBLIC_SCAPES, []).
+
+% --- --- --- API --- --- ---
+
+% A sync/1 function can compile and reload all the modules pertaining to the project within the
+% folder.
+sync() ->
+  make:all([load]).
+
+% The start/0 first checks whether a polis process has already been spawned, by checking if one is
+% registered. If it's not, then the start/1 function starts up the neuroevolutionary platform.
+start() ->
+  case whereis(polis) of
+    undefined ->
+      gen_server:start(?MODULE, {?MODS, ?PUBLIC_SCAPES}, []);
+    Polis_PId ->
+      io:format("Polis: ~p is already running on this node.~n", [Polis_PId])
+  end.
+
+start(Start_Parameters) ->
+  gen_server:start(?MODULE, Start_Parameters, []).
+
+init(PId, InitState) ->
+  gen_server:cast(PId, {init, InitState}).
+
+% The stop/0 function first checks whether a polis process is online. If there is an online polis
+% process running on the node, then the stop function sends a signal to it requesting it to stop.
+stop() ->
+  case whereis(polis) of
+    undefined ->
+      io:format("Polis cannot be stopped, it is not online.~n");
+    Polis_PId ->
+      gen_server:cast(Polis_PId, {stop, normal})
+  end.
+
+% --- --- --- gen_server callbacks --- --- ---
+
+% !!! Using cryptographically strong seed instead of erlang:now() and random:seed().
+% !!! reference: (see How to generate cryptographically strong seeds with the new rand library)
+% https://hashrocket.com/blog/posts/the-adventures-of-generating-random-numbers-in-erlang-and-elixir
+%
+% The init/1 function first seeds random with a new seed, in the case a random number generator will
+% be needed. The polis process is then registered, the mnesia database is started, and the supporting
+% modules, if any, are then started through the start_supmods/1 function. Then all the specified
+% public scapes, if any, are activated. Having called our neuroevolutionary platform polis, we give
+% this polis a name “MATHEMA”, which is a greek word for knowledge, and learning. Finally we create
+% the initial state, which contains the Pids of the currently active public scapes, and the names of
+% the activated mods. The function then drops into the main gen_server loop.
+init({Mods, PublicScapes}) ->
+  <<I1:32/unsigned-integer, I2:32/unsigned-integer, I3:32/unsigned-integer>> =
+    crypto:strong_rand_bytes(12),
+  rand:seed(exsplus, {I1, I2, I3}),
+  process_flag(trap_exit, true),
+  register(polis, self()),
+  io:format("Parameters: ~p~n", [{Mods, PublicScapes}]),
+  mnesia:start(),
+  start_supmods(Mods),
+  Active_PublicScapes = start_scapes(PublicScapes, []),
+  io:format("--- --- --- Polis: ##MATHEMA## is now online --- --- ---~n"),
+  InitState = #state{ active_mods=Mods, active_scapes=Active_PublicScapes },
+  % Scape_PIdsP = [{ScapePId, Scape_Type} ...]
+  {ok, InitState}.
+
+% At this point polis only accepts a get_scape call, to which it replies with the Pid or undefined
+% message, and the two standard {stop,normal} and {stop,shutdown} calls.
+handle_call({get_scape, Type}, {_Cx_PId, _Ref}, S) ->
+  Active_PublicScapes = S#state.active_scapes,
+  Scape_PId = case lists:keyfind(Type, 3, Active_PublicScapes) of
+    false ->
+      undefined;
+    PS ->
+      PS#scape_summary.address
+  end,
+  {reply, Scape_PId, S};
+handle_call({stop, normal}, _From, State) ->
+  {stop, normal, State};
+handle_call({stop, shutdown}, _From, State) ->
+  {stop, shutdown, State}.
+
+% At this point polis allows only for 3 standard casts: {init, InitState}, {stop, normal} and
+% {stop, shutdown}.
+handle_cast({init, InitState}, _State) ->
+  {noreply, InitState};
+handle_cast({stop, normal}, State) ->
+  {stop, normal, State};
+handle_cast({stop, shutdown}, State) ->
+  {stop, shutdown, State}.
+
+% The standard, still unused handle_info/2 function.
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+% When polis is terminated, it first shuts down all the scapes by calling stop_scapes/1, and then all
+% the supporting mods, by calling stop_supmods/1.
+terminate(Reason, S) ->
+  Active_Mods = S#state.active_mods,
+  stop_supmods(Active_Mods),
+  stop_scapes(S#state.active_scapes),
+  io:format(
+    "--- --- --- Polis: ##MATHEMA## is now offline, terminated with reason: ~p --- --- ---~n",
+    [Reason]
+  ),
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+% --- --- --- internal functions --- --- ---
+
+% The create/0 function sets up new mnesia databases composed of the dx, cortex, neuron, sensor,
+% actuator, polis, and population, and specie tables. The reset/0 function deletes the schema, and
+% recreates a fresh database from scratch.
+create() ->
+  mnesia:create_schema( [ node() ] ), 
+  mnesia:start(), 
+  mnesia:create_table(
+    population,
+    [
+      { disc_copies, [ node() ] },
+      { type, set },
+      { attributes, record_info( fields, population ) }
+    ]
+  ),
+  mnesia:create_table(
+    specie,
+    [
+      { disc_copies, [ node() ] },
+      { type, set },
+      { attributes, record_info( fields, specie ) }
+    ]
+  ),
+  mnesia:create_table(
+    agent,
+    [
+      { disc_copies, [ node() ] },
+      { type, set },
+      { attributes, record_info( fields, agent ) }
+    ]
+  ),
+  mnesia:create_table(
+    cortex,
+    [
+      { disc_copies, [ node() ] },
+      { type, set },
+      { attributes, record_info( fields, cortex ) }
+    ]
+  ),
+  mnesia:create_table(
+    neuron,
+    [
+      { disc_copies, [ node() ] },
+      { type, set },
+      { attributes, record_info( fields, neuron ) }
+    ]
+  ),
+  mnesia:create_table(
+    sensor,
+    [
+      { disc_copies, [ node() ] },
+      { type, set },
+      { attributes, record_info( fields, sensor ) }
+    ]
+  ),
+  mnesia:create_table(
+    actuator,
+    [
+      { disc_copies, [ node() ] },
+      { type, set },
+      { attributes, record_info( fields, actuator ) }
+    ]
+  ).
+
+reset() ->
+  mnesia:stop(),
+  ok = mnesia:delete_schema( [ node() ] ),
+  polis:create().
+
+% Start/Stop enviromental modules: DBs, Environments, Network Access systems and tools...
+
+% The start_supmods/1 function expects a list of module names of the mods that are to be started with
+% the startup of the neuroeovlutionary platform. Each module must have a start/0 function that starts
+% up the supporting mod process.
+start_supmods( [ ModName | ActiveMods ] ) ->
+  ModName:start(),
+  start_supmods( ActiveMods );
+start_supmods( [] ) ->
+  done.
+
+% The stop_supmods/1 expects a list of supporting mod names, the mod's name must be the name of its
+% module, and that module must have a stop/0 function tat stops the module. stop_supmods/1 goes
+% through the list of the mods, and stops each one.
+stop_supmods( [ ModName | ActiveMods ] ) ->
+  ModName:stop(),
+  stop_supmods(ActiveMods);
+stop_supmods( [] ) ->
+  done.
+
+% The start_scapes/2 function accepts a list of scape_summary records, which specify the names of the
+% public scapes and any parameters that with which those scapes should be started. What specifies
+% what scape that is going to be created by the scape module is the Type that is dropped into the
+% function. Ofcourse the scape module should already be able to create the Type of scape that is
+% dropped into the start_link function. Once the scape is started, we record the Pid in that
+% scape_summary's record. When all the public scapes have been started, the function outputs a list
+% of updated scape_summary records.
+start_scapes( [ S | Scapes ], Acc ) ->
+  Type = S#scape_summary.type,
+  Parameters = S#scape_summary.parameters,
+  { ok, PId } = scape:start_link( { self(), Type, Parameters } ),
+  start_scapes( Scapes, [ S#scape_summary{ address=PId } | Acc ] );
+start_scapes( [], Acc ) ->
+  lists:reverse( Acc ).
+
+% The stop_scapes/1 function accepts a list of scape_summary records. The function extracts the Pid
+% of the scape from the scape_summary, and requests for that scape to terminate itself.
+stop_scapes( [ S | Scapes ] ) ->
+  PId = S#scape_summary.address,
+  gen_server:cast( PId, { self(), stop, normal } ),
+  stop_scapes( Scapes );
+stop_scapes( [] ) ->
+  ok.
+
